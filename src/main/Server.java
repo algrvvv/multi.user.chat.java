e58d72525e52a1164fce77370038a43eb2f346fe
p4xt3r <alexandrgr25@gmail.com>
2024-05-09 13:19:27 +0300

    work on the server has been completed

--- src/main/Server.java
+++ src/main/Server.java
@@ -3,9 +3,17 @@ package main;
 import managers.server.MessageStore;
 import managers.server.ServerInstance;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.LinkedList;
 
 public class Server {
+    /**
+     * Порт для работа сервера
+     */
+    public static final int PORT = 8080;
+
     /**
      * Список всех экземпляров
      */
@@ -15,4 +23,19 @@ public class Server {
      * Хранилище последних сообщений
      */
     public static MessageStore store;
+
+    @SuppressWarnings("InfiniteLoopStatement")
+    public static void main(String[] args) throws IOException {
+        store = new MessageStore();
+        try (ServerSocket server = new ServerSocket(PORT)) {
+            while (true) {
+                Socket socket = server.accept();
+                try {
+                    serverInstances.add(new ServerInstance(socket));
+                } catch (IOException e) {
+                    socket.close();
+                }
+            }
+        }
+    }
 }

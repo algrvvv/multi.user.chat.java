15ab164e6bacdb9f290ac509de322728c8a15d77
p4xt3r <alexandrgr25@gmail.com>
2024-05-10 17:29:48 +0300

    added setting for the maximum number of simultaneous users
    client side shutdown doesn't work yet

--- src/main/Server.java
+++ src/main/Server.java
@@ -24,15 +24,38 @@ public class Server {
      */
     public static MessageStore store;
 
+    /**
+     * Максимально возможное количество одновременно подключенных пользователей
+     */
+    public static int maxNumberOfUser = 10;
+
     @SuppressWarnings("InfiniteLoopStatement")
     public static void main(String[] args) throws IOException {
         System.out.println("Запуск севера");
         store = new MessageStore();
+        if (args.length > 0) {
+           try {
+               for (int i = 0; i < args.length; i++) {
+                   if (args[i].equals("-m") || args[i].equals("--max")) {
+                       maxNumberOfUser = Integer.parseInt(args[++i]);
+                   }
+               }
+           } catch (Exception e) {
+               System.out.println("Ошибка конфигурации сервера");
+               return;
+           }
+        }
+
         try (ServerSocket server = new ServerSocket(PORT)) {
             while (true) {
                 Socket socket = server.accept();
                 try {
-                    serverInstances.add(new ServerInstance(socket));
+                    if (serverInstances.size() + 1 <= maxNumberOfUser) {
+                        serverInstances.add(new ServerInstance(socket));
+                    } else {
+                        System.out.println("Ошибка подключения нового пользователя в связи с ограничением максимального кол-ва пользователей");
+                        socket.close();
+                    }
                 } catch (IOException e) {
                     socket.close();
                 }

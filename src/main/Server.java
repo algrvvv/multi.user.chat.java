55afa63d5bb30fac8e57a775916206b85a04c575
p4xt3r <alexandrgr25@gmail.com>
2024-05-11 13:50:32 +0300

    enabled use of server command class

--- src/main/Server.java
+++ src/main/Server.java
@@ -1,9 +1,12 @@
 package main;
 
 import managers.server.MessageStore;
+import managers.server.ServerCommand;
 import managers.server.ServerInstance;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.LinkedList;
@@ -25,9 +28,19 @@ public class Server {
     public static MessageStore store;
 
     /**
+     * Чтение команд на сервере
+     */
+    private static BufferedReader serverCommandReader;
+
+    /**
      * Максимально возможное количество одновременно подключенных пользователей
      */
-    public static int maxNumberOfUser = 10;
+    public static int MAX_NUMBER_OF_USERS = 10;
+
+    /**
+     * Максимально возможное количество сообщения в хранилище
+     */
+    public static int MAX_NUMBER_OF_MESSAGE = 10;
 
     /**
      * Колво активных пользователей
@@ -37,12 +50,14 @@ public class Server {
     @SuppressWarnings("InfiniteLoopStatement")
     public static void main(String[] args) throws IOException {
         System.out.println("Запуск севера");
-        store = new MessageStore();
+        serverCommandReader = new BufferedReader(new InputStreamReader(System.in, "Cp866"));
+        store = new MessageStore(MAX_NUMBER_OF_MESSAGE);
+
         if (args.length > 0) {
            try {
                for (int i = 0; i < args.length; i++) {
                    if (args[i].equals("-m") || args[i].equals("--max")) {
-                       maxNumberOfUser = Integer.parseInt(args[++i]);
+                       MAX_NUMBER_OF_USERS = Integer.parseInt(args[++i]);
                    }
                }
            } catch (Exception e) {
@@ -51,17 +66,16 @@ public class Server {
            }
         }
 
+        new ServerCommand(Server.serverCommandReader).start();
+
         try (ServerSocket server = new ServerSocket(PORT)) {
             while (true) {
                 Socket socket = server.accept();
                 try {
-                    if (numberOfUser < maxNumberOfUser) {
-                        System.out.println(getNumberOfUser());
+                    if (numberOfUser < MAX_NUMBER_OF_USERS) {
                         serverInstances.add(new ServerInstance(socket));
                         incrementNumberOfUser();
-                        System.out.println(getNumberOfUser());
                     } else {
-                        System.out.println(getNumberOfUser());
                         System.out.println("Ошибка подключения нового пользователя в связи с ограничением максимального кол-ва пользователей");
                         socket.close();
                     }
